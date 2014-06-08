dbaf1e9bf38eb4827c5c2fc6ad505f23f37e575d
David Figueroa <diego.david.figueroa@gmail.com>
2014-06-08 18:27:05 -0300

    Updated Test Engine to execute pattern recognition for a set of cascade classifiers

--- workspace/TestEngine/src/main/PatternDetector.java
+++ workspace/TestEngine/src/main/PatternDetector.java
@@ -1,20 +1,50 @@
 package main;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.objdetect.CascadeClassifier;
 
-public abstract class PatternDetector {
+public class PatternDetector {
 	
-	protected CascadeClassifier classifier;
+	private CascadeClassifier classifier;
 	protected Mat image;
 	protected MatOfRect detections;
+	
+	public PatternDetector(Mat image) {
+		this.image = image;
+		this.detections = new MatOfRect();
+	}
 	
 	public PatternDetector(Mat image, CascadeClassifier classifier) {
 		this.classifier = classifier;
 		this.image = image;
 		this.detections = new MatOfRect();
+	}
+	
+	public PatternDetector(Mat image, CascadeClassifier classifier1, CascadeClassifier classifier2) {
 		
+	}
+	
+	public MatOfRect Detection(){
+		this.detections = new MatOfRect();
+		this.classifier.detectMultiScale(this.image, this.detections);
+		return this.detections;
+	}
+	
+	public Mat DrawRectangle(){
+		Mat im = this.image;
+		for (Rect rect : detections.toArray()) {
+			   Core.rectangle(im, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+		}
+		return im;
+	}
+	
+	public int DetectedPatterns() {
+		return this.detections.toArray().length;
 	}
 
 }
--- workspace/TestEngine/src/main/TestEngine.java
+++ workspace/TestEngine/src/main/TestEngine.java
@@ -5,7 +5,11 @@ import java.io.File;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 public class TestEngine {
@@ -16,7 +20,6 @@ public class TestEngine {
 	
 	
 	  public static void main(String[] args) {
-		    System.out.println("Hello, OpenCV");
 
 		    // Load the native library.
 		    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -24,19 +27,51 @@ public class TestEngine {
 		    File faceCascadeDir = new File(faceCascadeClassifierPath);
 		    String faceCascadeClassifiers[] = faceCascadeDir.list();
 		    for(int i=0; i<faceCascadeClassifiers.length; i++) {
-		    	CascadeClassifier faceDetector = new CascadeClassifier(faceCascadeDir.getAbsolutePath() + "\\" + faceCascadeClassifiers[i]);
-		    	File eyeCascadeDir = new File(eyeCascadeClassifierPath);
-			    String eyeCascadeClassifiers[] = eyeCascadeDir.list();
-			    for(int j=0; j<eyeCascadeClassifiers.length; j++) {
-			    	CascadeClassifier eyeDetector = new CascadeClassifier(eyeCascadeDir.getAbsolutePath() + "\\" + eyeCascadeClassifiers[j]);
+		    	long startTime = System.currentTimeMillis();
+		    	CascadeClassifier faceClassifier = new CascadeClassifier(faceCascadeDir.getAbsolutePath() + "\\" + faceCascadeClassifiers[i]);
+		    	//File eyeCascadeDir = new File(eyeCascadeClassifierPath);
+			    //String eyeCascadeClassifiers[] = eyeCascadeDir.list();
+			    //for(int j=0; j<eyeCascadeClassifiers.length; j++) {
+			    	//CascadeClassifier eyeClassifier = new CascadeClassifier(eyeCascadeDir.getAbsolutePath() + "\\" + eyeCascadeClassifiers[j]);
 			    	File imagesDir = new File(imagesPath);
 			    	String images[] = imagesDir.list();
+			    	int detections = 0;
 			    	for(int k=0; k<images.length; k++) {
 			    		Mat image = Highgui.imread(imagesDir.getAbsolutePath() + "\\" + images[k]);
-			    		FaceDetector face = new FaceDetector(image, faceDetector);
-			    		MatOfRect detectedFaces = face.FaceDetection();
+			    		
+			    		// Convert to gray scale
+			    		Mat gray = new Mat();
+			    		Imgproc.cvtColor(image, gray, Imgproc.COLOR_RGB2GRAY);
+			    		 
+			    		PatternDetector faceDetector = new PatternDetector(gray, faceClassifier);
+			    		MatOfRect faceDetections = faceDetector.Detection();
+			    		//System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+			    		
+			    		for (Rect rect : faceDetections.toArray()) {
+			    			   Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+			    			   detections++;
+			    			   //Mat face = new Mat(image, rect);
+			    			   //PatternDetector eyeDetector = new PatternDetector(face, eyeClassifier);
+			    			   //MatOfRect eyeDetections = eyeDetector.Detection();
+			    			   
+			    			   //System.out.println(String.format("Detected %s eyes", eyeDetections.toArray().length));
+			    			   //for (Rect rect2 : eyeDetections.toArray()) {
+			    				   //Core.rectangle(image, new Point(rect.x + rect2.x, rect.y + rect2.y), new Point(rect.x + rect2.x + rect2.width, rect.y + rect2.y + rect2.height), new Scalar(255, 0, 0));
+			    			   //}
+			    			}
+			    		
+			    		// Save the visualized detection.
+			    		String filename = faceCascadeClassifiers[i] + " - " + images[k] + ".png";
+			    		//System.out.println(String.format("Writing %s", filename));
+			    		Highgui.imwrite(filename, image);
+			    		
+			    		
+			    		
 			    	}
-			    }
+			    	long stopTime = System.currentTimeMillis();
+			        long elapsedTime = (stopTime - startTime)*1000;
+			    	System.out.println(String.format("Detected %d faces using %s in %d seconds", detections, faceCascadeClassifiers[i], elapsedTime));
+			    //}
 		    }
 		    
 		    
