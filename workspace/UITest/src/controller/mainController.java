31657e90c1bccecb7610f53c6768a5ce6c42f720
David Figueroa <diego.david.figueroa@gmail.com>
2014-08-24 20:14:08 -0300

    Arreglada carga múltiple

--- workspace/UITest/src/controller/mainController.java
+++ workspace/UITest/src/controller/mainController.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -36,7 +37,8 @@ public class mainController implements Initializable{
 	
 	private String path;
 	
-	private List<File> imageFiles;
+	private List<File> imageFiles = new ArrayList<File>();
+	private List<Boolean> detected = new ArrayList<Boolean>();
 			
 	@FXML
 	Parent root;
@@ -64,13 +66,14 @@ public class mainController implements Initializable{
 		         new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif")
 		         );
 		//File selectedFile = fileChooser.showOpenDialog(application.Main.stage);
-		
-		imageFiles.addAll(fileChooser.showOpenMultipleDialog(application.Main.stage)); // get the list of image files  
-		for (File file : imageFiles) {  
+		List<File> files = fileChooser.showOpenMultipleDialog(application.Main.stage);
+		imageFiles.addAll(files);
+		for (File file : files) {  
 				     ImageView imageView;
 					try {
 						imageView = createImageView(file);
 						imageContainer.getChildren().add(imageView);
+						detected.add(false);
 						
 					} catch (FileNotFoundException e) {
 						// TODO Auto-generated catch block
@@ -90,24 +93,29 @@ public class mainController implements Initializable{
 	
 	@FXML
 	public void Detect() {
-		
-		for (Node n : imageContainer.getChildren()) {  
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Detector det = new Detector();
-		det.setImagePath(path);
-		Image img = det.detect();
+		int i = 0;
+		for (File file : imageFiles) {  
+			if (detected.get(i).equals(false)) {
+			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+			Detector det = new Detector();
+			det.setImagePath(file.getAbsolutePath());
+			Image img = det.detect();
+			detected.set(i, true);
 		//imageContainer.getChildren().remove(file);
 		//imageView.setImage(img);
-		
+			}
+		i++;
 		}
 	}
 	
 	private ImageView createImageView(final File imageFile) throws FileNotFoundException {  
-	     // DEFAULT_THUMBNAIL_WIDTH is a constant you need to define  
-	     // The last two arguments are: preserveRatio, and use smooth (slower) resizing  
-	     Image image = new Image(new FileInputStream(imageFile), 100, 0, true, true) ;  
-	     ImageView imageView = new ImageView(image);  
+		final int DEFAULT_THUMBNAIL_WIDTH = 100;
+	     final Image image = new Image(new FileInputStream(imageFile)) ;  
+	     final ImageView imageView = new ImageView(image);  
+	     imageView.setFitWidth(DEFAULT_THUMBNAIL_WIDTH);  
+	     imageView.setPreserveRatio(true);  
+	     imageView.setSmooth(true);  
 	     return imageView ;  
-	}
+	}  
 
 }
